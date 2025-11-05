d721c6c182527fc94c59681253fb99c0ded26378
Duda-Braga <mecb@cesar.org.br>
2025-11-05 12:08:34 -0300

    check shipping adress and add to cart

--- mobile-flow/pages/product_page.py
+++ mobile-flow/pages/product_page.py
@@ -5,7 +5,14 @@ class Product(BasePage):
     class Locators:
         SEARCH_BAR_FIELD = (AppiumBy.XPATH, "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.ImageView")
         FIRST_PRODUCT_RECOMEDATION = (AppiumBy.XPATH, "//android.view.View[contains(@content-desc, 'R$')][1]")
-    
+        TYPE_CEP_FIELD = (AppiumBy.XPATH, '//android.widget.EditText[@resource-id="Digite o CEP"]')
+        ERROR_MSG = (AppiumBy.XPATH, "//android.view.View[@content-desc='Campo obrigatório']")
+        CALCULATE_FRETE_BTN = (AppiumBy.XPATH, "//android.widget.Button[@content-desc='Calcular']")
+        CLEAR_FRETE_BTN = (AppiumBy.XPATH, "//android.widget.Button[@content-desc='Apagar cep pesquisado']")
+        FRETE_PRICE_MSG = (AppiumBy.XPATH, "//android.view.View[contains(@content-desc, 'Receba em até ')]")
+        BUY_BTN = (AppiumBy.XPATH, "//android.view.View[@content-desc='comprar']")
+
+
     def __init__(self,driver):
         super().__init__(driver)
 
@@ -16,3 +23,56 @@ class Product(BasePage):
     def is_product_price_correct(self, price):
         locator = (AppiumBy.XPATH,f"//*[@content-desc and contains(@content-desc, '{price}')]")
         return self.find_element(*locator)
+    
+    def find_zip_field(self):
+        self.scroll_untill_is_visible(*self.Locators.TYPE_CEP_FIELD, "down", 2)
+
+    def fill_invalid_zip_code(self):
+        self.click_element(*self.Locators.TYPE_CEP_FIELD)
+        self.send_keys_to_element(*self.Locators.TYPE_CEP_FIELD, "123456")
+        self.click_element(*self.Locators.CALCULATE_FRETE_BTN)
+
+    def fill_valid_zip_code(self, code):
+        self.click_element(*self.Locators.TYPE_CEP_FIELD)
+        self.send_keys_to_element(*self.Locators.TYPE_CEP_FIELD, code)
+        self.click_element(*self.Locators.CALCULATE_FRETE_BTN)
+
+    def clear_zip_code_field(self):
+        return self.click_element(*self.Locators.CLEAR_FRETE_BTN)
+
+    def is_error_message_on_screen(self):
+        return self.find_element(*self.Locators.ERROR_MSG)
+    
+    def is_delivery_time_on_screen(self):
+        return self.find_element(*self.Locators.FRETE_PRICE_MSG)
+    
+    def get_shipping_cost(self):
+        full_text = self.get_content_desc(*self.Locators.FRETE_PRICE_MSG)
+        if full_text == "":
+            return ""
+        parts = full_text.split(': ')
+        
+        if len(parts) > 1:
+            price_part = parts[1].strip()
+            if "R$" in price_part:
+                price_parts = price_part.split("R$")
+                return price_parts[-1].strip()
+            else: # freee
+                return price_part         
+        return ""
+            
+    def get_delivery_time(self):
+        full_text = self.get_content_desc(*self.Locators.FRETE_PRICE_MSG)
+        if full_text == "":
+            return ""
+        parts = full_text.split(': ')
+        
+        if len(parts) > 1:
+            time_part = parts[0].split("até ")
+            if len(time_part) > 1:
+                time = time_part[-1]
+                return time   
+        return ""
+    
+    def click_on_buy_product(self):
+        self.click_element(*self.Locators.BUY_BTN)
--- mobile-flow/tests/scenario3.py
+++ mobile-flow/tests/scenario3.py
@@ -22,6 +22,9 @@ def test_product_purchase_flow(driver, data_set):
     product_price = data_set['Price']
     product_name = data_set['Product']
     product_delivery = data_set['delivery_estimate']
+    product_zipcode = data_set['Zipcode']
+    product_shippingfee = data_set['shipping_fee']
+
 
     home = Home(driver)
     assert home.is_on_home_screen(), "App did not launch correctly"
@@ -35,7 +38,17 @@ def test_product_purchase_flow(driver, data_set):
     assert product.is_product_name_correct(product_name), "Name is not correspoding according to the API response"
     assert product.is_product_price_correct(product_price), "Price is not correspoding according to the API response"
     
+    product.find_zip_field()
+    product.fill_invalid_zip_code()
+    assert product.is_error_message_on_screen(), "The error message did not appear when entering the incorrect zip code"
+    product.clear_zip_code_field()
+    product.fill_valid_zip_code(product_zipcode)
+    assert product.is_delivery_time_on_screen(), "The delivery time should appear when entering a correct zip code"
+    
+    assert product.get_delivery_time() == product_delivery, f"Delivery time is not correspoding according to the API response. It shpuld be {product_delivery}, but it is {product.get_delivery_time()}"
+    assert product.get_shipping_cost() == product_shippingfee, f"Shipping cost is not correspoding according to the API response. It shpuld be {product_shippingfee}, but it is {product.get_shipping_cost()}"
 
+    product.click_on_buy_product()
     time.sleep(3)
 
 # 1. Open App: Launch the Americanas application.
@@ -43,11 +56,10 @@ def test_product_purchase_flow(driver, data_set):
 # 3. Select Product: Tap on the desired product in the search results.
 # 4. Validate Product Page:
 # Confirm that the product name and price are correct according to the API response.
-
-
 # Enter an invalid ZIP code, click "Calculate", and verify that an error message is displayed.
 # Enter the valid ZIP code returned by the API and validate the delivery time and shipping cost.
 # 5. Add to Cart: Tap the "Buy" button.
+
 # 6. Validate Cart Popup:
 # In the cart popup, confirm the product name and price again.
 # Increase the quantity to 2 and check if the quantity field is updated.
